358e8315804be61addd4dff0f1da4c200f7bfef2
FAVRE Jessie (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC HOURCADE GLA) <8009383b@commun.ad.sncf.fr>
2023-01-10 15:31:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,221 @@
+// imports
+const express = require('express');
+const { Client } = require('pg');
+require('dotenv').config()
+
+
+// declarations
+const app = express();
+const port = 8000;
+const client = new Client({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME, 
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+});
+
+client.connect();
+
+// for parsing application/json
+app.use(express.json());
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// routes
+
+app.get('/api/tickets', async (req, res) => {
+    try {
+        const data = await client.query('SELECT * FROM tickets ORDER BY created_at');
+
+        res.status(200).json(
+            {
+                status: 'OK',
+                message: 'liste des tickets',
+                data: data.rows,
+            });
+    }
+    catch (err) {
+        res.status(500).json(
+            {
+                status: "fail",
+                message: 'erreur serveur'
+            }
+        )
+        console.log(err.stack)
+    }
+})
+
+
+app.get('/api/tickets/:id', async (req, res) => {
+    console.log(req.params)
+    const id = req.params.id;
+
+    try {
+        const data = await client.query('SELECT * FROM tickets where id = $1', [id]);
+
+        if (data.rows.length === 1) {
+            res.status(200).json(
+                {
+                    status: "ok",
+                    data: data.rows,
+
+                }
+            )
+        }
+        else {
+            res.status(404).json(
+                {
+                    status: "fail",
+                    message: "cet id ne correspond à aucun ticket"
+                });
+        }
+
+    }
+    catch (err) {
+        res.status(500).json(
+            {
+                status: "fail",
+                message: "erreur serveur"
+            }
+        )
+        console.log(err.stack)
+    }
+})
+
+
+app.post('/api/tickets', async (req, res) => {
+    console.log(req.body);
+    const message = req.body.message;
+
+    try {
+        const data = await client.query('INSERT INTO tickets (message) VALUES ($1) RETURNING *', [message]);
+        if (message) {
+            res.status(201).json(
+                {
+                    status: "ok",
+                    message: "ticket créé",
+                    data: data.rows
+                }
+            )
+
+        } else {
+            res.status(404).json(
+                {
+                    status: "fail",
+                    message: "Aucun message, écrire un message"
+                });
+
+        }
+
+    }
+    catch (err) {
+        res.status(500).json(
+            {
+                status: "fail",
+                message: "erreur serveur"
+            }
+        )
+        console.log(err.stack)
+    }
+})
+
+
+app.delete('/api/tickets/:id', async (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    if (isNaN(id) == true) {
+        res.status(406).json(
+            {
+                status:"fail",
+                message: "entrer un nombre",
+            }
+        )
+    }
+    try {
+        const data = await client.query('DELETE FROM tickets WHERE id = $1', [id]);
+
+        if (data.rowCount === 1) {
+            res.status(200).json(
+                {
+                    status: "ok deleted",
+                    message: "ticket effacé",
+                });
+
+
+        }
+        else {
+            res.status(404).json(
+                {
+                    status: "fail",
+                    message: "id ticket invalide"
+                });
+        }
+
+    
+}
+    catch (err) {
+        res.status(500).json(
+            {
+                status: "fail",
+                message: "erreur serveur"
+            }
+        )
+        console.log(err.stack)
+    };
+})
+
+app.put('/api/tickets/:id', async (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    const {message, done} = req.body ;
+    console.log(message, done);
+    
+    try{
+        const ticket = await client.query('SELECT * FROM tickets WHERE id = $1', [id]);
+    
+        const data = await client.query('UPDATE tickets SET done = $2 WHERE id = $1 RETURNING *', [id, done]);
+        if (data.rowCount === 1) {
+            res.status(201).json(
+                {
+                    status: "succes",
+                    data: data.rows[0]
+                });
+    }
+    else {
+        res.json({ done: false })
+    }
+}
+catch (err) {
+    res.status(500).json(
+        {
+            status: "fail",
+            message: "erreur serveur"
+        }
+    )
+    console.log(err.stack)
+}
+})
+
+// ecoute le port 8000
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
